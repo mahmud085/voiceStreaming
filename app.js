7a5995cdeef0b3ce8b4b13bed2da852a979af3d9
mahmud <mahmud@dhrubokinfotech.com>
2016-07-26 17:53:37 +0600

    3rd commit

--- app.js
+++ app.js
@@ -12,15 +12,53 @@ app.use(express.static(__dirname +'/public'));
 app.get('/',function(req,res){
 	console.log("here");
 	res.redirect('home.html');
-})
+});
+var usernames = {};
+var rooms = ['public','room1','room2'];
+var cr = {};
 io.on('connection',function(socket){
-	socket.on('room',function(room){
-		socket.join(room);
-		socket.emit('join',room);
+	socket.on('adduser',function(username){
+		// store the username in the socket session for this client
+		socket.username = username;
+		// store the room name in the socket session for this client
+		socket.room = 'public';
+		cr.name = socket.room;
+		// add the client's username to the global list
+		usernames[username] = username;
+		// send client to room 1
+		socket.join('public');
+		// echo to client they've connected
+		//socket.emit('updatechat', 'SERVER', 'you have connected to channel public');
+		// echo to room 1 that a person has connected to their room
+		io.sockets.in('public').emit('updatechat', 'SERVER', username + ' has joined this channel',socket.room);
+		socket.emit('updaterooms', rooms, 'public');
+		socket.emit('current_room',socket.room);
+	});
+	socket.on('switchRoom', function(newroom){
+		// sent message to OLD room
+		io.sockets.in(socket.room).emit('updatechat', 'SERVER', socket.username+' has left this channel',socket.room);
+		// leave the current room (stored in session)
+		socket.leave(socket.room);
+		// join new room, received as function parameter
+		socket.join(newroom);
+		socket.room = newroom;
+		cr.name = socket.room;
+		console.log("socket 3",socket.room);
+		io.sockets.in(newroom).emit('updatechat', 'SERVER', socket.username+' has joined this channel',socket.room);
+		socket.emit('updaterooms', rooms, newroom);
+		socket.emit('current_room',socket.room);
+	});
+	socket.on('addRoom',function(room){
+		rooms.push(room);
+	});
+	socket.on('roomname',function(a){
+		console.log("socket 2",cr.name);
+		socket.emit('room',cr.name);
 	});
 	socket.on('voice',function(blob){
+		console.log("socket ",cr.name);
 		//socket.broadcast.emit('voice',blob);
-		io.sockets.in('audio').emit('voice',blob);
+		io.sockets.in(cr.name).emit('voice',blob);
 	});
 });
 
--- public/broadcast.js
+++ public/broadcast.js
@@ -1,22 +1,31 @@
 var mediaRecorder;
 var socket = io();
+var cr;
+$(function(){
+	socket.on('connect', function(){
+		    // call the server-side function 'adduser' and send one parameter (value of prompt)
+	socket.emit('adduser1', prompt("What's your name?"));
+	});
+});
 $('#start').click(function(){
-		var constraints = { audio: true };
-		navigator.mediaDevices.getUserMedia(constraints).then(function(mediaStream) {
-	    mediaRecorder = new MediaRecorder(mediaStream);
-	    mediaRecorder.onstart = function(e) {
-	        this.chunks = [];
-	    };
-	    mediaRecorder.ondataavailable = function(e) {
-	        this.chunks.push(e.data);
-	    };
-	    mediaRecorder.onstop = function(e) {
-	        var blob = new Blob(this.chunks, { 'type' : 'audio/ogg; codecs=opus' });
-	        socket.emit('voice', blob);
-	    };
+	prompt("What's your name?");
+	var constraints = { audio: true };
+	navigator.mediaDevices.getUserMedia(constraints).then(function(mediaStream) {
+    mediaRecorder = new MediaRecorder(mediaStream);
+    mediaRecorder.onstart = function(e) {
+        this.chunks = [];
+    };
+    mediaRecorder.ondataavailable = function(e) {
+        this.chunks.push(e.data);
+    };
+    mediaRecorder.onstop = function(e) {
+        var blob = new Blob(this.chunks, { 'type' : 'audio/ogg; codecs=opus' });
+        console.log("socket room : ",cr);
+        socket.emit('voice', blob);
+    };
 
-	    // Start recording
-	    mediaRecorder.start();
+    // Start recording
+    mediaRecorder.start();
 
 	});
 });
